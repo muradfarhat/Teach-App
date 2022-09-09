d10494aae1d46d0d8f972774b126b8c9d989a984
Murad Farhat <92052407+muradfarhat@users.noreply.github.com>
2022-09-09 15:20:37 +0300

    firstPage

--- lib/firstPage.dart
+++ lib/firstPage.dart
@@ -9,6 +9,9 @@ class firstPage extends StatefulWidget {
 
 class _firstPageState extends State<firstPage> {
   Color basicColor = const Color.fromARGB(255, 246, 63, 63);
+
+  int selected = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +24,7 @@ class _firstPageState extends State<firstPage> {
           child: Column(
         children: [
           Container(
-            padding: const EdgeInsets.symmetric(vertical: 40),
+            padding: const EdgeInsets.only(top: 70, bottom: 70),
             width: double.infinity,
             alignment: Alignment.center,
             child: const Text(
@@ -29,6 +32,81 @@ class _firstPageState extends State<firstPage> {
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
           ),
+          Row(
+            children: [
+              Expanded(
+                  child: Container(
+                alignment: Alignment.center,
+                height: 200,
+                child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        selected = 1;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: selected == 1
+                                  ? Colors.blue
+                                  : Colors.red.shade400,
+                              width: 3),
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                      width: 150,
+                      height: 150,
+                    )),
+              )),
+              Expanded(
+                  child: Container(
+                alignment: Alignment.center,
+                height: 200,
+                child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        selected = 2;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: selected == 2
+                                  ? Colors.blue
+                                  : Colors.red.shade400,
+                              width: 3),
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                      width: 150,
+                      height: 150,
+                    )),
+              ))
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 100),
+            width: 135,
+            alignment: Alignment.center,
+            //margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 45),
+            child: MaterialButton(
+              color: basicColor,
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 15),
+                    child: const Text("إبدأ اللعب",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
+                  ),
+                  const Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       )),
     );
